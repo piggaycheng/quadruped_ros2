83704fd1993258cf7a3811876e79703f3345b43a
Yu Cheng <yucheng@mirle.com.tw>
2025-11-25 13:21:28 +0800

    refactor: rename and update tanh_post_process function to directly scale input data to target range

--- quadruped/utils/action.py
+++ quadruped/utils/action.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 
-def tanh_process(data: np.ndarray, limit: tuple[float, float]):
-    # 使用 tanh 將 data 從 (-inf, inf) 映射到 (-1, 1)
-    tanh_data = np.tanh(data)
+def tanh_post_process(data: np.ndarray, limit: tuple[float, float]):
     # 將 (-1, 1) 的範圍縮放到目標範圍 [min, max]
     data_min, data_max = limit
     data_range = (data_max - data_min) / 2.0
     data_bias = (data_max + data_min) / 2.0
-    scaled_data = tanh_data * data_range + data_bias
+    scaled_data = data * data_range + data_bias
     return scaled_data
